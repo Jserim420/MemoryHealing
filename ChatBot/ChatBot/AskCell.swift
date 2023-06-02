f7ff79a92b4418d6fb6bc84ef054d06ee4953a63
Jserim420 <diworl102@gmail.com>
2023-06-02 12:38:03 +0900

    no message

---
+++ ChatBot/ChatBot/AskCell.swift
@@ -0,0 +1,12 @@
+import UIKit
+
+/// Hosts a text field for the user to enter a question
+class AskCell: UITableViewCell {
+
+    @IBOutlet weak var textField: UITextField!
+    
+//    let view = UIView()
+//    view.layer.borderWidth = 2.0
+//    view.layer.borderColor = UIColor.red.cgColor
+//    view.layer.cornerRadius = 10.0
+}
